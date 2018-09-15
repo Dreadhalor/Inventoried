b9fb41552eee3aa8b0ab346c685cdc1bcd697a12
Scott Hetrick <Scott.Hetrick@la-archdiocese.org>
2018-09-15 01:25:52 -0700

    Testing if experimental sql-client change will work

--- node_modules/@dreadhalor/sql-client/dist/db/scripts/generated/database/create_database_Inventoried.sql
+++
@@ -1,9 +0,0 @@
---======================================
---  Create Database Template
---======================================
-USE [master]
-
-IF NOT EXISTS (SELECT * FROM sys.databases WHERE name LIKE 'Inventoried')
-BEGIN
-  CREATE DATABASE [Inventoried]
-END
